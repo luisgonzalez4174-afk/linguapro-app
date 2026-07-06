dbb72f34d19bd1ab1858395185257425b8f217db
LuisFranco13 <luisfranco.200312@gmail.com>
2026-07-06 14:55:14 -0600

    feat: user registration with name, visual vocab cards with audio button, permanent progress

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'linguapro-v9';
+const CACHE = 'linguapro-v10';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
