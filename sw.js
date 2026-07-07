6da0b2cfef5001039d2ba9607ac9cd191d7415ba
LuisFranco13 <luisfranco.200312@gmail.com>
2026-07-07 14:47:49 -0600

    fix: audio chips legibles (sin fondo morado), scroll al tope en cada pantalla

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'linguapro-v12';
+const CACHE = 'linguapro-v13';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
